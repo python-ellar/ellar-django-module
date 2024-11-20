33a29c2f13ab8f27cdeef21dc7ddb5474b7e209a
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2024-11-20 06:40:53 +0100

    updated sample exercise

--- sample/ellar-and-django-orm/ellar_and_django_orm/root_module.py
+++ sample/ellar-and-django-orm/ellar_and_django_orm/root_module.py
@@ -6,7 +6,7 @@ from ellar.common import (
     exception_handler,
 )
 from ellar.core import ModuleBase
-from ellar.di import Container
+from ellar.di import ProviderConfig
 
 from ellar_django import DjangoModule
 
@@ -18,14 +18,12 @@ from .interfaces.events_repository import IEventRepository
     modules=[
         DjangoModule.setup(settings_module="ellar_and_django_orm.wsgi_django.settings"),
         EventModule,
-    ]
+    ],
+    providers=[
+        ProviderConfig(IEventRepository, use_class="ellar_and_django_orm.services.event_repository:EventRepository")
+    ],
 )
 class ApplicationModule(ModuleBase):
     @exception_handler(404)
     def exception_404_handler(cls, ctx: IExecutionContext, exc: Exception) -> Response:
         return JSONResponse({"detail": "Resource not found."}, status_code=404)
-
-    def register_services(self, container: Container) -> None:
-        from .services.event_repository import EventRepository
-
-        container.register(IEventRepository, EventRepository)
